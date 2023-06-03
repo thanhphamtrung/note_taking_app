903dd4771a8bf7ec9099b7e3dbf526402fad8db8
thanhphamtrung <thanhpt22emu@gmail.com>
2023-06-03 15:51:00 +0700

    Create app_language.dart

---
+++ lib/src/constants/app_language.dart
@@ -0,0 +1,16 @@
+class Authentication {
+  static String appTitle = 'Note Application';
+  static String email = 'Email';
+  static String password = 'Password';
+  static String confirmPassword = 'Confirm Password';
+  static String loginBtnLabel = 'Login';
+  static String signUpBtnLabel = 'Sign Up';
+  static String resetPasswordBtnLabel = 'Reset Password';
+  static String forgotPasswordLabel = 'Forgot Password?';
+  static String signInWithLabel = 'Or sign in with:';
+  static String redirectToLoginLabel = 'Already have an account? Login';
+  static String backToLoginLabel = 'Back to Login';
+  static String googleLabel = 'Google';
+  static String facebookLabel = 'Facebook';
+  static String createAccountLabel = 'Create an account';
+}
